436f53b31f87e4771a52937723e6e8709d59ccdb
Bradley Bossard <bradleybossard@gmail.com>
2014-03-10 23:44:45 +0000

    Added writing of template to a string, than an output file

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
   "text/template"
+  "bytes"
   "os"
-  //"fmt"
 )
 
 type CfdgParams struct {
@@ -12,8 +12,28 @@ type CfdgParams struct {
 
 
 func main() {
-  params := CfdgParams{1.0}
+  // Read template
   tmpl,err := template.ParseFiles("./tmpl/circle-branch.cfdg")
-  err = tmpl.Execute(os.Stdout, params)
   if err != nil { panic(err) }
+
+  // Create instance of struct and buffer to write template out to
+  params := CfdgParams{1.0}
+  var tmplBuf bytes.Buffer;
+
+  // Execute template and put contents in a string
+  err = tmpl.Execute(&tmplBuf, params)
+  if err != nil { panic(err) }
+  tmplString := tmplBuf.String() 
+
+  outputName := "test0.cfdg"
+  // Open file, close on program exit and write complete template out
+  fo, err := os.Create("output/" + outputName) 
+  if err != nil { panic(err) }
+  defer func() {
+      if err := fo.Close(); err != nil {
+          panic(err)
+      }
+  }()
+
+  fo.WriteString(tmplString)
 }
