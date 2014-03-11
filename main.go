51d165c89fbeb2847455c2f38ad5253d4028b2e8
Bradley Bossard <bradleybossard@gmail.com>
2014-03-11 00:27:57 +0000

    Added loop for creating a series of templates

--- main.go
+++ main.go
@@ -4,12 +4,29 @@ import (
   "text/template"
   "bytes"
   "os"
+  "fmt"
 )
 
 type CfdgParams struct {
   Rotation1 float32
 }
 
+func writeTmplString(filename string, output string) {
+  // Open file, close on program exit and write complete template out
+  fo, err := os.Create("output/" + filename) 
+  if err != nil { panic(err) }
+/*
+  defer func() {
+      if err := fo.Close(); err != nil {
+          panic(err)
+      }
+  }()
+*/
+  fo.WriteString(output)
+
+  err = fo.Close();
+  if err != nil { panic(err) }
+}
 
 func main() {
   // Read template
@@ -20,20 +37,16 @@ func main() {
   params := CfdgParams{1.0}
   var tmplBuf bytes.Buffer;
 
-  // Execute template and put contents in a string
-  err = tmpl.Execute(&tmplBuf, params)
-  if err != nil { panic(err) }
-  tmplString := tmplBuf.String() 
 
-  outputName := "test0.cfdg"
-  // Open file, close on program exit and write complete template out
-  fo, err := os.Create("output/" + outputName) 
-  if err != nil { panic(err) }
-  defer func() {
-      if err := fo.Close(); err != nil {
-          panic(err)
-      }
-  }()
+  for i := 0; i <= 10; i++ {
+    // Execute template and put contents in a string
+    err = tmpl.Execute(&tmplBuf, params)
+    if err != nil { panic(err) }
+    tmplString := tmplBuf.String() 
 
-  fo.WriteString(tmplString)
+    outputName := fmt.Sprintf("test%05d.cfdg", i)
+    writeTmplString(outputName, tmplString) 
+  }
+
+
 }
