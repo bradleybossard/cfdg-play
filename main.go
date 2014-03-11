1e5118681a76cc1faee764d39f3665bb63cb6831
Bradley Bossard <bradleybossard@gmail.com>
2014-03-11 01:14:46 +0000

    Fixed bug where template was being appended to self, not refreshed

--- main.go
+++ main.go
@@ -11,17 +11,12 @@ type CfdgParams struct {
   Rotation1 float32
 }
 
+// Open file, close on program exit and write complete template out
 func writeTmplString(filename string, output string) {
-  // Open file, close on program exit and write complete template out
+
   fo, err := os.Create("output/" + filename) 
   if err != nil { panic(err) }
-/*
-  defer func() {
-      if err := fo.Close(); err != nil {
-          panic(err)
-      }
-  }()
-*/
+
   fo.WriteString(output)
 
   err = fo.Close();
@@ -35,10 +30,11 @@ func main() {
 
   // Create instance of struct and buffer to write template out to
   params := CfdgParams{1.0}
-  var tmplBuf bytes.Buffer;
-
 
   for i := 0; i <= 10; i++ {
+    var tmplBuf bytes.Buffer;
+    params.Rotation1 = float32(i) * 0.2
+
     // Execute template and put contents in a string
     err = tmpl.Execute(&tmplBuf, params)
     if err != nil { panic(err) }
