54b3c379d7a9110c534bc7321220b7e85a06fc9a
Bradley Bossard <bradleybossard@gmail.com>
2014-03-11 01:15:42 +0000

    Added code to make front to back gif

--- make_cfdg.sh
+++ make_cfdg.sh
@@ -3,3 +3,9 @@ for infile in $files; do
   outfile=img/`basename $infile .cfdg`.png
   cfdg $infile $outfile 
 done;
+
+files=`find img | grep png | sort`
+backfiles=`find img | grep png | sort -r`
+backforthfiles=`echo $files $backfiles`
+gifname=`basename $(echo $files | cut -d" " -f1) .png`.gif
+convert -delay 10 -loop 0 $backforthfiles img/$gifname
